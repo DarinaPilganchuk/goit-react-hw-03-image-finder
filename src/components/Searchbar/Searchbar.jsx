06402dca38376235186d7ad3e893dfe0644c5573
DarinaPilganchuk <106483724+DarinaPilganchuk@users.noreply.github.com>
2023-01-28 20:55:04 +0200

    відкориговано компонет форми, налаштовано очищення

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,49 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Searchbar.css';
+
 import { ImSearch }  from 'react-icons/im';
 
 
-class Searchbar extends Component {
+export class Searchbar extends Component {
   state = {
-    inputData: '',
-  };
-  onChangeInput = event => {
-    this.setState({ inputData: event.currentTarget.value.toLowerCase() });
+    searchQuery: '',
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state.inputData);
-    this.setState({ inputData: '' });
+  handleChange = e => {
+  
+    this.setState({ searchQuery: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.searchQuery.trim() === '') {
+      return alert('Please enter something :)');
+    }
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ searchQuery: '' });
   };
 
   render() {
-    const { inputData } = this.state.inputData;
     return (
       <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="SearchForm">
           <button type="submit" className="SearchForm-button">
-            <ImSearch size={25} />
+            <span>
+              <ImSearch size={25} stroke="#3f51b5" />
+            </span>
           </button>
 
           <input
-            className="SearchForm-input"
-            name="inputData"
-            value={inputData}
-            onChange={this.onChangeInput}
+            className="SearchForm-input "
             type="text"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            name="searchQuery"
+            value={this.state.searchQuery}
+            onChange={this.handleChange}
           />
         </form>
       </header>
     );
   }
 }
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
 
 export default Searchbar;
-Searchbar.propType = {
-  onSubmit: PropTypes.func.isRequired,
-};
